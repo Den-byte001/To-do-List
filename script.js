f1e716072c57553422db2c5917f99a083bac6671
Dennis <timilehinoyaniyi6@gmail.com>
2026-09-25 20:13:41 +0100

    Writing the functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// let textInput = document.getElementById("inputText");
+// const addBtn = document.getElementById("addBtn");
+// const deleteBtn = document.getElementById("delBtn");
+// const unorderList = document.getElementById("unorderList");
+// const eachList = document.getElementsByClassName("eachList");
+// const delBtn = document.getElementsByClassName("delBtn");
+// let textDiv = document.getElementsByClassName("textDiv");
+
+// addBtn.addEventListener("click", () => {
+//   const listLine = document.createElement("li");
+//   listLine = textInput.value;
+//   unorderList.appendChild(listLine);
+// });
