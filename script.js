c3fb2c1ee4b736b8ae4847d4b3a9c43f97adce01
Dennis <timilehinoyaniyi6@gmail.com>
2026-09-25 20:52:44 +0100

    Added creating new list

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 "use strict";
 
-// let textInput = document.getElementById("inputText");
-// const addBtn = document.getElementById("addBtn");
+const textInput = document.getElementById("inputText");
+const addBtn = document.getElementById("addBtn");
 // const deleteBtn = document.getElementById("delBtn");
-// const unorderList = document.getElementById("unorderList");
-// const eachList = document.getElementsByClassName("eachList");
-// const delBtn = document.getElementsByClassName("delBtn");
-// let textDiv = document.getElementsByClassName("textDiv");
+const unorderList = document.getElementById("unorderList");
+const eachList = document.getElementsByClassName("eachList");
+const delBtn = document.getElementsByClassName("delBtn");
+const textDiv = document.getElementsByClassName("textDiv");
 
-// addBtn.addEventListener("click", () => {
-//   const listLine = document.createElement("li");
-//   listLine = textInput.value;
-//   unorderList.appendChild(listLine);
-// });
+addBtn.addEventListener("click", () => {
+  const newList = document.createElement("li");
+  newList.textContent = textInput.value;
+
+  if (newList) {
+    newList.className = eachList.className;
+  }
+
+  unorderList.appendChild(newList);
+
+  textInput.value = "";
+});
